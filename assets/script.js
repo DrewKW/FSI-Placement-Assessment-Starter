2d58264f3139292cc090e3822c56d8df98f3f1dd
DrewKW <97498427+DrewKW@users.noreply.github.com>
2022-02-18 10:34:23 -0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -6,70 +6,87 @@ let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
 
+let TotalQuantity = gb + cc + sugar;
 
-function updateTotal (displayTotal) {
-    let total = document.querySelector("#qty-total");
-    total.textContent = displayTotal;
+function updateOrderSummary(displayGb) {
+    let  totalGb = document.querySelector('#qty-gb');
+    totalGb.textContent = displayGb;
+}
+
+function updateOrderSummary2(displaycc) {
+    let  totalcc = document.querySelector('#qty-cc');
+    totalcc.textContent = displaycc;
+}
+
+function updateOrderSummary3(displaysugar) {
+    let  totalsugar = document.querySelector('#qty-sugar');
+    totalsugar.textContent = displaysugar;
+}
+
+function updateOrderSummary4(displayTotalQuantity) {
+    let TotalQuantity = document.querySelector('#qty-total');
+    TotalQuantity.textContent = displayTotalQuantity;
 }
 
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit');
 
-// selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb');
-const gbMinusBtn = document.querySelector('#minus-gb');
-
-// selecting the element with an id of add-cc
-const ccPlusBtn = document.querySelector('#add-cc');
-const ccMinusBtn = document.querySelector('#minus-cc');
-
-// selecting the element with an id of add-sugar
-const sugarPlusBtn = document.querySelector('#add-sugar');
-const sugarMinusBtn = document.querySelector('#minus-sugar');
-
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
 
+// selecting the element with an id of add-gb
+const gbPlusBtn = document.querySelector('#add-gb');
+const gbMinusBtn = document.querySelector('#minus-gb');
+const ccPlusBtn = document.querySelector('#add-cc');
+const ccMinusBtn = document.querySelector('#minus-cc');
+const sugarPlusBtn = document.querySelector('#add-sugar');
+const sugarMinusBtn = document.querySelector('#minus-sugar');
+const TotalQtyBtn = document.querySelector('#qty-total');
 
 // Event listener for clicks on the "-" button for Gingerbread cookies
-gbMinusBtn.addEventListener('click', function (e) {
+gbMinusBtn.addEventListener('click', function (_e) {
     if (gb > 0) {
         gb--;
-        // updateTotal = document.querySelector(".qty-gb");
-    // total.textContent = display
+        TotalQuantity--;
+        updateOrderSummary(`${gb}`);
+        updateOrderSummary4(`${TotalQuantity}`);
     }
     });
 gbPlusBtn.addEventListener('click', function (e) {
         gb++;
-        console.log(gb);
-    //     updateTotal = document.querySelector(".qty-total");
-    // total.textContent = displayTotal;
+        TotalQuantity++; 
+        updateOrderSummary(`${gb}`); 
+        updateOrderSummary4(`${TotalQuantity}`); 
 });
 
 // Event listener for clicks on the "-" and "+" buttons for Chocolate Chip cookies
-ccMinusBtn.addEventListener('click', function (e) {
+ccMinusBtn.addEventListener('click', function (_e) {
     if (cc > 0) {
         cc--;
-        updateTotal = document.querySelector(".qty-total");
-    total.textContent = displayTotal;
+        TotalQuantity--;
+        updateOrderSummary2(`${cc}`);
+        updateOrderSummary4(`${TotalQuantity}`);
     }
 });
 ccPlusBtn.addEventListener('click', function (e) {
         cc++;
-        updateTotal = document.querySelector(".qty-total");
-    total.textContent = displayTotal;
+        TotalQuantity++;
+        updateOrderSummary2(`${cc}`);
+        updateOrderSummary4(`${TotalQuantity}`);
 });
 
 // Event listener for clicks on the "-" and "+" buttons for Sugar Sprinkle cookies
-sugarMinusBtn.addEventListener('click', function (e) {
+sugarMinusBtn.addEventListener('click', function (_e) {
     if (sugar > 0) {
         sugar--;
-        updateTotal = document.querySelector(".qty-total");
-    total.textContent = displayTotal;
+        TotalQuantity--;
+        updateOrderSummary3(`${sugar}`);
+        updateOrderSummary4(`${TotalQuantity}`);
     }
 });
-sugarPlusBtn.addEventListener('click', function (e) {
+sugarPlusBtn.addEventListener('click', function (_e) {
         sugar++;
-        updateTotal = document.querySelector(".qty-total");
-    total.textContent = displayTotal;
+        TotalQuantity++;
+        updateOrderSummary3(`${sugar}`);
+        updateOrderSummary4(`${TotalQuantity}`);
 });
